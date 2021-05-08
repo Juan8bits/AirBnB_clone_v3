92e55eaa4d498dd5186b47603927a467ef1b4b06
juan8bits <juan160820gmail.com>
2021-05-07 22:46:03 -0500

    Change output representation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,27 @@
 """ Script that returns a JSON: "status": "OK". """
 from api.v1.views import app_views
 from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status')
 def return_status():
     """ Return status of the page. """
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/stats')
 def return_stats():
-    """ Retrieves the number of each objects by type """
-    return storage.count()
+    """ Retrieves the number of each objects by type. """
+    characters = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    res = {}
+    for key, value in characters.items():
+        res.update({key: storage.count(value)})
+    return jsonify(res)
